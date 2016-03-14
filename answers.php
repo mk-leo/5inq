57ca7b7ce7ea1f99cc4639e163831bb82055f40b
LEONARD Michael <mic.leonard77@gmail.com>
2016-03-14 15:20:06 +0100

    Added files via upload

---
+++ answers.php
@@ -0,0 +1,20 @@
+<?php
+	echo "
+		A 4<br />
+		B 4<br />
+		C 1<br />
+		D 3<br />
+		E 4<br />
+		F 5<br />
+		G je ne sais pas<br />
+		H 3 je ne vois pas l'autre réponse<br />
+		I 4<br />
+		J 5<br />
+		K 3<br />
+		L 1<br />
+		M je ne sais pas<br />
+		N je ne sais pas
+	";
+
+
+?>
